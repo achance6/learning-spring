57b50a6eb7afbfb68cfdf35592bd40964196a6ad
Ayden Chance <achance6@jhu.edu>
2024-06-07 10:41:09 -0400

    added another entity and entity repository

---
+++ src/main/java/com/aydenchance/lil/learning_spring/data/GuestRepository.java
@@ -0,0 +1,9 @@
+package com.aydenchance.lil.learning_spring.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long> {
+
+}
