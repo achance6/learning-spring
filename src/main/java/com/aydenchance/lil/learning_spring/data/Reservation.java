756fb6de4642e43c25e68a6da906f7f3ad606f33
Ayden Chance <achance6@jhu.edu>
2024-06-07 10:53:42 -0400

    Created reservation entity

---
+++ src/main/java/com/aydenchance/lil/learning_spring/data/Reservation.java
@@ -0,0 +1,55 @@
+package com.aydenchance.lil.learning_spring.data;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="RESERVATION")
+public class Reservation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="RESERVATION_ID")
+	private Long id;
+	@Column(name="ROOM_ID")
+	private Long RoomId;
+	@Column(name="GUEST_ID")
+	private Long GuestId;
+	@Column(name="RES_DATE")
+	private Date date;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getRoomId() {
+		return RoomId;
+	}
+	public void setRoomId(Long roomId) {
+		RoomId = roomId;
+	}
+	public Long getGuestId() {
+		return GuestId;
+	}
+	public void setGuestId(Long guestId) {
+		GuestId = guestId;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	@Override
+	public String toString() {
+		return "Reservation [id=" + id + ", RoomId=" + RoomId + ", GuestId=" + GuestId + ", date=" + date + "]";
+	}
+	
+}
