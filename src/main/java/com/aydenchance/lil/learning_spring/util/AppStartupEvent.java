2ec5f35886fcb554901e7bde7c9f3c8880fb93e1
Ayden Chance <achance6@jhu.edu>
2024-06-07 10:44:24 -0400

    added getters/setters/toString to guest.java and added code to print each guest at app startup

--- src/main/java/com/aydenchance/lil/learning_spring/util/AppStartupEvent.java
+++ src/main/java/com/aydenchance/lil/learning_spring/util/AppStartupEvent.java
@@ -6,20 +6,26 @@ import org.springframework.stereotype.Component;
 
 import com.aydenchance.lil.learning_spring.data.Room;
 import com.aydenchance.lil.learning_spring.data.RoomRepository;
+import com.aydenchance.lil.learning_spring.data.Guest;
+import com.aydenchance.lil.learning_spring.data.GuestRepository;
 
 @Component
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
 
 	private final RoomRepository roomRepository;
+	private final GuestRepository guestRepository;
 	
-	public AppStartupEvent(RoomRepository roomRepository) {
+	public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository) {
 		this.roomRepository = roomRepository;
+		this.guestRepository = guestRepository;
 	}
 	
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
 		Iterable<Room> rooms = this.roomRepository.findAll();
 		rooms.forEach(System.out::println);
+		Iterable<Guest> guests = this.guestRepository.findAll();
+		guests.forEach(System.out::println);
 		
 	}
 
